735d3a7012df8088a8a5b0779fbf24b6693b6ec8
Giugno <giugnofabrizio@gmail.com>
2018-03-18 11:27:55 +0100

    update

--- home/static/js/nav.js
+++
@@ -1,8 +0,0 @@
-$( '.navbar-nav a' ).on( 'click', function () {
-	$( '.navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
-	$( this ).parent( 'li' ).addClass( 'active' );
-});
-
-$( '.navbar-nav a' ).on( 'click', function () {
-  $(alert("Hello! I am an alert box!!");
-}
