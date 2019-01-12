a62d930fa1d1100eb2eb53657340fafcb8b2e9d8
Giugno <giugnofabrizio@gmail.com>
2019-01-12 17:13:34 +0100

    css

--- cofiren/settings.py
+++ cofiren/settings.py
@@ -127,4 +127,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = BASE_DIR + '/static/'
+STATIC_ROOT = "/var/www/cofiren.cleverapps.io/static/"
+#STATIC_URL = BASE_DIR + '/static/'
